689868731f50a74741b558b498c3f4084dea7cfb
bitweeder <bitweeder@users.noreply.github.com>
2019-09-01 15:43:56 -0700

    Doxygen formatting, part 1

--- include/lucenaBAL/lucenaBAL.hpp
+++ include/lucenaBAL/lucenaBAL.hpp
@@ -19,15 +19,56 @@
 #pragma once
 
 
-//	Configure our public and private build options. These must be consistent
-//	between an executable and any libraries it uses.
+/**
+	@defgroup lbal_usage Usage Instructions
+
+	Generally, all that’s needed in order to make use of lucenaBAL is to
+	issue `#include <lucenaBAL/lucenaBAL.hpp>` in each target file. As a
+	header-only library with no dependencies beyond a nominal inclusion of
+	`<ciso646>` from the C++ Standard Library, there’s nothing else to link or
+	install. Additionally, there’s nothing much to configure, though you can
+	see [Configuration](@ref lbal_config) for details.
+
+	The library itself comprises a large collection of preprocessor macros,
+	grouped loosely into [build-time descriptors](@ref lbal_build_env),
+	[feature identifiers](@ref lbal_features), and
+	[linker decorators](@ref lbal_decorators). See the relevant sections for
+	more information.
+
+	@{
+		@defgroup lbal_structure Header Organization
+
+		lucenaBAL comprises a small number of public headers and some number of
+		private headers. Generally, only the public `<lucenaBAL/lucenaBAL.hpp>`
+		header needs to be directly referenced. Additionally, there is:
+
+			- `<lucenaBAL/lbalConfig.hpp>`, which is used to configure the
+			library as described in [Configuration](@ref lbal_config)
+			- `<lucenaBAL/lbalFeatureSetup.hpp>`, which describes most
+			[platform](@ref lbal_platform) and [compiler](@ref lbal_compiler)
+			features, [build-time descriptors](@ref lbal_build_env), and the
+			[linker decorators](@ref lbal_decorators)
+			- `<lucenaBAL/lbalVersion.hpp>`, which sets up
+			[Standard Library feature detection](@ref lbal_platform) macros.
+			These supplement whatever may have been set up by
+			`lbalFeatureSetup.hpp`; the two sets are not fully orthogonal, as
+			the build environment may influence the availability of certain
+			Standard Library features.
+
+		The private headers are included by these as necessary, and should
+		never be invoked directly.
+	@}
+	@defgroup lbal_config Configuration
+	@defgroup lbal_build_env Build-time Environment
+	@defgroup lbal_features Feature Detection Macros
+	@{
+		@defgroup lbal_platform Platform Features
+		@defgroup lbal_compiler Compiler Features
+		@defgroup lbal_library Standard Library Features
+	@}
+	@defgroup lbal_decorators Linker Decorators
+*/
+
 #include <lucenaBAL/lbalConfig.hpp>
-
-//	Handle feature detection and set up build environment macros.
 #include <lucenaBAL/lbalFeatureSetup.hpp>
-
-//	Set up Standard Library feature detection macros. These supplement whatever
-//	may have been set up by lbalFeatureSetup.hpp; the two sets are not
-//	orthogonal, as the build environment may influence the availability of
-//	certain Standard Library features. See lbalVersion.hpp for details.
 #include <lucenaBAL/lbalVersion.hpp>
